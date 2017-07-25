742c8a2857daabba9c8991a8600baf7e9ad63991
colmconn <colmconn+github@gmail.com>
2017-07-25 10:37:02 -0700

    Use loops instead of hard coding everything.

--- 10-cluster-all-grief-regressions.sh
+++ 10-cluster-all-grief-regressions.sh
@@ -13,46 +13,16 @@ export OMP_NUM_THREADS=40
 
 if [[ ${do_baseline_to_followup_change_regressions} -eq 1 ]] ; then
     
-    ####################################################################################################
-    ### relativeVsStanger
-    ####################################################################################################
+    for glt in "relativeVsStanger relativeGriefVsRelativeNeutral relativeGriefVsStrangerGrief" ; do
+    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
+				      -x cc  -g ${glt} -e delta.grief.scaled -i ${glt}.analysis.one.delta.grief.scaled
 
     ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
-				      -x cc  -g relativeVsStanger -e delta.grief.scaled -i relativeVsStanger.analysis.one.delta.grief.scaled
+				      -x cc  -g ${glt} -e delta.grief.scaled -i ${glt}.analysis.two.delta.grief.scaled.and.grief.delta.hamd
 
     ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
-				      -x cc  -g relativeVsStanger -e delta.grief.scaled -i relativeVsStanger.analysis.two.delta.grief.scaled.and.grief.delta.hamd
-
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
-				      -x cc  -g relativeVsStanger -e delta.grief.scaled -i relativeVsStanger.analysis.three.delta.grief.scaled.and.age
-
-
-    ####################################################################################################
-    ### relativeGriefVsRelativeNeutral
-    ####################################################################################################
-
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
-				      -x cc  -g relativeGriefVsRelativeNeutral -e delta.grief.scaled -i relativeGriefVsRelativeNeutral.analysis.one.delta.grief.scaled
-
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
-				      -x cc  -g relativeGriefVsRelativeNeutral -e delta.grief.scaled -i relativeGriefVsRelativeNeutral.analysis.two.delta.grief.scaled.and.grief.delta.hamd
-
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
-				      -x cc  -g relativeGriefVsRelativeNeutral -e delta.grief.scaled -i relativeGriefVsRelativeNeutral.analysis.three.delta.grief.scaled.and.age
-
-
-    ####################################################################################################
-    ### relativeGriefVsStrangerGrief
-    ####################################################################################################
-
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
-				      -x cc  -g relativeGriefVsStrangerGrief -e delta.grief.scaled -i relativeGriefVsStrangerGrief.analysis.one.delta.grief.scaled
-
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
-				      -x cc  -g relativeGriefVsStrangerGrief -e delta.grief.scaled -i relativeGriefVsStrangerGrief.analysis.two.delta.grief.scaled.and.grief.delta.hamd
-
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
-				      -x cc  -g relativeGriefVsStrangerGrief -e delta.grief.scaled -i relativeGriefVsStrangerGrief.analysis.three.delta.grief.scaled.and.age
+				      -x cc  -g ${glt} -e delta.grief.scaled -i ${glt}.analysis.three.delta.grief.scaled.and.age
+    done
 
 
     ####################################################################################################
@@ -79,45 +49,22 @@ fi
 
 if [[ ${do_baseline_only_regressions} -eq 1 ]] ; then
 
-####################################################################################################
-    ### relativeVsStanger
-    ####################################################################################################
+    for variable in grief grief.a grief.b grief.c iri_pt iri_ec ; do
 
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/baseline.regressions/ \
-				      -x cc  -g relativeVsStanger -e grief -i relativeVsStanger.baseline.analysis.one.grief
+	for glt in relativeVsStanger relativeGriefVsRelativeNeutral relativeGriefVsStrangerGrief ; do
 
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/baseline.regressions/ \
-				      -x cc  -g relativeVsStanger -e grief -i relativeVsStanger.baseline.analysis.two.grief.and.hamd
-
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/baseline.regressions/ \
-				      -x cc  -g relativeVsStanger -e grief -i relativeVsStanger.baseline.analysis.three.grief.and.age
-
-
-    ####################################################################################################
-    ### relativeGriefVsRelativeNeutral
-    ####################################################################################################
-
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/baseline.regressions/ \
-				      -x cc  -g relativeGriefVsRelativeNeutral -e grief -i relativeGriefVsRelativeNeutral.baseline.analysis.one.grief
-
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/baseline.regressions/ \
-				      -x cc  -g relativeGriefVsRelativeNeutral -e grief -i relativeGriefVsRelativeNeutral.baseline.analysis.two.grief.and.hamd
-
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/baseline.regressions/ \
-				      -x cc  -g relativeGriefVsRelativeNeutral -e grief -i relativeGriefVsRelativeNeutral.baseline.analysis.three.grief.and.age
-
-
-    ####################################################################################################
-    ### relativeGriefVsStrangerGrief
-    ####################################################################################################
-
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/baseline.regressions/ \
-				      -x cc  -g relativeGriefVsStrangerGrief -e grief -i relativeGriefVsStrangerGrief.baseline.analysis.one.grief
-
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/baseline.regressions/ \
-				      -x cc  -g relativeGriefVsStrangerGrief -e grief -i relativeGriefVsStrangerGrief.baseline.analysis.two.grief.and.hamd
-
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/baseline.regressions/ \
-				      -x cc  -g relativeGriefVsStrangerGrief -e grief -i relativeGriefVsStrangerGrief.baseline.analysis.three.grief.and.age
-
+	    echo "####################################################################################################"
+	    echo "### Variable: ${variable} GLT: ${glt}"
+	    echo "####################################################################################################"	    
+	    
+	    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/baseline.regressions/ \
+					      -x cc  -g ${glt} -e ${variable} -i ${glt}.baseline.analysis.one.${variable}
+	    exit
+	    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/baseline.regressions/ \
+					      -x cc  -g ${glt} -e ${variable} -i ${glt}.baseline.analysis.two.${variable}.and.hamd
+	    
+	    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/baseline.regressions/ \
+					      -x cc  -g ${glt} -e ${variable} -i ${glt}.baseline.analysis.three.${variable}.and.age
+	done
+    done
 fi
