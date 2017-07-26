656bb60019752816a2dba93ac5cf32f591e50854
colmconn <colmconn+github@gmail.com>
2017-07-26 11:11:22 -0700

    Loopify the follow-up clustering commands.

--- 10-cluster-all-grief-regressions.sh
+++ 10-cluster-all-grief-regressions.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+trap exit SIGHUP SIGINT SIGTERM
+
 set -x
 
-do_baseline_to_followup_change_regressions=0
-do_baseline_only_regressions=1
+do_baseline_to_followup_change_regressions=1
+do_baseline_only_regressions=0
 
 export OMP_NUM_THREADS=40
 
@@ -12,33 +14,36 @@ export OMP_NUM_THREADS=40
 ####################################################################################################
 
 if [[ ${do_baseline_to_followup_change_regressions} -eq 1 ]] ; then
-    
-    for glt in "relativeVsStanger relativeGriefVsRelativeNeutral relativeGriefVsStrangerGrief" ; do
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
-				      -x cc  -g ${glt} -e delta.grief.scaled -i ${glt}.analysis.one.delta.grief.scaled
 
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
-				      -x cc  -g ${glt} -e delta.grief.scaled -i ${glt}.analysis.two.delta.grief.scaled.and.grief.delta.hamd
+    for variable in delta.grief.scaled delta.grief.a.scaled delta.grief.b.scaled delta.grief.c.scaled  ; do
+	for glt in relativeVsStanger relativeGriefVsRelativeNeutral relativeGriefVsStrangerGrief ; do
+	    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/followup.regressions/ \
+					      -x cc  -g ${glt} -e ${variable} -i ${glt}.followup.analysis.one.${variable}
 
-    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
-				      -x cc  -g ${glt} -e delta.grief.scaled -i ${glt}.analysis.three.delta.grief.scaled.and.age
-    done
-
+	    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/followup.regressions/ \
+					      -x cc  -g ${glt} -e ${variable} -i ${glt}.followup.analysis.two.${variable}.and.delta.hamd.scaled
+	    
+	    ./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/followup.regressions/ \
+					      -x cc  -g ${glt} -e ${variable} -i ${glt}.followup.analysis.three.${variable}.and.age
+	done
+	
 
     ####################################################################################################
     ### rg and sg
     ####################################################################################################
 
-    for ff in rg sg ; do
-
-	./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
-					  -x cc  -g ${ff} -e delta.grief.scaled -i ${ff}.analysis.one.delta.grief.scaled
-
-	./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
- 					  -x cc  -g ${ff} -e delta.grief.scaled -i ${ff}.analysis.two.delta.grief.scaled.and.grief.delta.hamd
-
-	./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
- 					  -x cc  -g ${ff} -e delta.grief.scaled -i ${ff}.analysis.three.delta.grief.scaled.and.age
+	# for ff in rg sg ; do
+	
+	# 	./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
+	    # 					  -x cc  -g ${ff} -e -e ${variable} -i ${glt}.followup.analysis.one.${variable}
+	
+	# 	./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
+	    # 					  -x cc  -g ${ff} -e ${variable} -i ${glt}.followup.analysis.two.${variable}.and.delta.hamd
+	
+	# 	./09-cluster-grief-regressions.sh -p 0.01 -c 0.05 -n 1 -s 2  -d ../Group.data/ -r ../Group.results/Grief/regressions/ \
+	    # 					  -x cc  -g ${ff} -e ${variable} -i ${glt}.followup.analysis.three.${variable}.and.age
+	# done
+	
     done
 fi
 
